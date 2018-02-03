d1b68b6c6f6cfa61f3bff422ced0e40bafdb4a5a
Vu Hoang Minh <ginjewel103@gmail.com>
2018-02-03 08:57:03 +0700

    updated ver

--- 08.practical.work.pthread.c
+++ 08.practical.work.pthread.c
@@ -71,9 +71,10 @@ int main(){
 pthread_t tid1,tid2;
 
 pthread_create(&tid1,NULL,thread_create,NULL);
-pthread_join(tid1,NULL);
-
 pthread_create(&tid2,NULL,thread_consume,NULL);
+
+
+pthread_join(tid1,NULL);
 pthread_join(tid2,NULL);
 return 0;
 }
