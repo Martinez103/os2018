e1c716a0e7393e11e38690a105431248785df519
Vu Hoang Minh <ginjewel103@gmail.com>
2018-02-08 16:36:12 +0700

    reupload 10

--- 10.practical.work.semaphore.c
+++ 10.practical.work.semaphore.c
@@ -7,102 +7,82 @@
 #include <semaphore.h>
 
 #define BUFFER_SIZE 10
-/// define semaphore
+///////// SEMAPHORE /////////
 sem_t s;
 
 typedef struct{
-char type;
+int type;
 int amount;
-char unit;
+int unit;
 } item;
-
 item buffer[BUFFER_SIZE];
+
 int first=0;
 int last=0;
 
 void produce(item *i){
-	while ((first + 1) % BUFFER_SIZE == last) {
-		return;
-	}
 	sem_wait(&s);
+	while ((first + 1) % BUFFER_SIZE == last) {
+		
+	}
 	memcpy(&buffer[first], i, sizeof(item));
 	first = (first + 1) % BUFFER_SIZE;
 	sem_post(&s);
 }
+
 item *consume() {
 	sem_wait(&s);
 	item *i = malloc(sizeof(item));
 	while (first == last) {
-		return;
+		
 	}
 	memcpy(i, &buffer[last], sizeof(item));
 	last = (last + 1) % BUFFER_SIZE;
 	sem_post(&s);
 	return i;
-	
 }
 
-item *create(char x,int y,char z){
-item *curr = malloc(sizeof(item));
-curr->type = x;
-curr->amount = y;
-curr->unit = z;
-return curr;
+void display(item *x){
+	printf("%d %d %d\n",x->type,x->amount,x->unit);
 }
 
-void display(item *i){
-if(i=NULL)
-  return;
-else
-printf("%d %d %d",i->type,i->amount,i->unit);
+item create_item(int x,int y,int z){
+	item curr;
+	curr.type = x;
+	curr.amount = y;
+	curr.unit = z;
+	return curr;
 }
 
-
-
-
-void *thread_create(void *param){
-     item *i1,*i2,*i3;
-i1 = create(0,4,3);
-i2 = create(1,5,3);
-i3 = create(7,3,4);
-
-produce(i1);
-produce(i2);
-produce(i3);
-
+void *thread_producer(void *param){
+	item i1,i2,i3;
+	i1 = create_item(1,3,4);
+	i2 = create_item(5,7,6);
+	i3 = create_item(3,1,1);
+	item *p1,*p2,*p3;		
+	p1 = &i1;	
+	p2 = &i2;
+	p3 = &i3;	
+	produce(p1);
+	produce(p2);
+	produce(p3);
 }
 
-void *thread_consume(void *param){
-
-display(consume());
-display(consume());
-
+void *thread_consumer(void *param){
+	display(consume());
+	display(consume());	
 }
 
 
 int main(){
-sem_init(&s,0,1);
-pthread_t tid1,tid2;
+	sem_init(&s,0,1);
+	pthread_t tid1,tid2;
+	pthread_create(&tid1,NULL,thread_producer,NULL);
+	pthread_create(&tid2,NULL,thread_consumer,NULL);
 
-pthread_create(&tid1,NULL,thread_create,NULL);
-pthread_create(&tid2,NULL,thread_consume,NULL);
-
-pthread_join(tid1,NULL);
-pthread_join(tid2,NULL);
-
-return 0;
+	pthread_join(tid1,NULL);
+	pthread_join(tid2,NULL);
+	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
